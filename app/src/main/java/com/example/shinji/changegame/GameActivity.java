b8753293835806575f02831c8391db227b1ed426
KIRIN3 <scomputer13@gmail.com>
2017-09-08 16:57:17 +0900

    Start

---
+++ app/src/main/java/com/example/shinji/changegame/GameActivity.java
@@ -0,0 +1,18 @@
+package com.example.shinji.changegame;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by shinji on 2017/09/07.
+ */
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+    }
+}
