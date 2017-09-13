deaf1c03d7ddf6c4b0f6d38107094c4c16cbe846
KIRIN3 <scomputer13@gmail.com>
2017-09-13 13:56:27 +0900

    レイアウトを２つに変更

--- app/src/main/java/com/example/shinji/changegame/CoinMng.java
+++ app/src/main/java/com/example/shinji/changegame/CoinMng.java
@@ -1,5 +1,10 @@
 package com.example.shinji.changegame;
 
+import android.content.Context;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
 /**
  * Created by shinji on 2017/09/12.
  */
@@ -15,5 +20,29 @@ public class CoinMng {
 
 
 
+    public static void CoinInit(Context context,LinearLayout layout){
 
+        float xy[] = CommonMng.PsToPx(10,80);
+        Log.w( "DEBUG", "aaa x[] " + xy[0]);
+        Log.w( "DEBUG", "aaa y[] " + xy[1]);
+
+        // ImageViewオブジェクトの作成
+        ImageView imageView = new ImageView(context);
+
+        // 画像の設定
+        imageView.setImageResource(R.drawable.ichien);
+
+        // タッチリスナーの設定
+//        imageView.setOnTouchListener(context);
+
+        // 画像のサイズの設定
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(150,150);
+
+        // 表示座標の設定
+        lp.leftMargin = (int)xy[0];
+        lp.topMargin = (int)xy[1];
+
+        // 画像の追加
+        layout.addView(imageView,lp);
+    }
 }
--- app/src/main/java/com/example/shinji/changegame/GameActivity.java
+++ app/src/main/java/com/example/shinji/changegame/GameActivity.java
@@ -7,7 +7,6 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
-
 /**
  * Created by shinji on 2017/09/07.
  */
@@ -20,29 +19,11 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
 
 
-
-        float xy[] = CommonMng.PsToPx(10,90);
-        Log.w( "DEBUG", "aaa x[] " + xy[0]);
-        Log.w( "DEBUG", "aaa y[] " + xy[1]);
-
         // 表示したいレイアウト
         LinearLayout layout = (LinearLayout)findViewById(R.id.over_layout);
 
-        // ImageViewオブジェクトの作成
-        ImageView imageView = new ImageView(this);
+        CoinMng.CoinInit(this,layout);
 
-        // 画像の設定
-        imageView.setImageResource(R.drawable.ichien);
-
-        // 画像のサイズの設定
-        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(150,150);
-
-        // 表示座標の設定
-        //lp.leftMargin = (int)xy[0];
-        //lp.topMargin = (int)xy[1];
-
-        // 画像の追加
-        layout.addView(imageView,lp);
 
     }
 }
