0d5e32b8c4366b52907fb0e8c3895db8222e723b
KIRIN3 <scomputer13@gmail.com>
2017-09-17 10:55:43 +0900

    外部でのTouchに成功

--- app/src/main/java/com/example/shinji/changegame/CoinMng.java
+++ app/src/main/java/com/example/shinji/changegame/CoinMng.java
@@ -2,17 +2,31 @@ package com.example.shinji.changegame;
 
 import android.app.Activity;
 import android.content.Context;
+import android.support.v7.widget.AppCompatButton;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import java.util.ArrayList;
+import android.view.View.OnTouchListener;
+
 
 /**
  * Created by shinji on 2017/09/12.
  */
 
-public class CoinMng extends GameActivity{
+public class CoinMng implements OnTouchListener{
+
+    private static Context mContext;
+    private static ViewGroup mViewGroup;
+    private static LinearLayout mLayout;
+    private static View.OnTouchListener mVOL;
 
     // コイン種類
     static String coinType[] = {"1yen","5yen","10yen","50yen","100yen","500yen","1000yen"};
@@ -22,53 +36,68 @@ public class CoinMng extends GameActivity{
 
     static int ichienImageId = R.drawable.ichien;
 
-    public static void CoinInit(Context context,LinearLayout layout){
+    public CoinMng(Context context,LinearLayout layout) {
+        mContext = context;
+        mLayout = layout;
+   //     mVOL = vol;
+    }
 
-        float xy[] = CommonMng.PsToPx(10,80);
+    public void CoinInit(){
+
+        float xy[] = CommonMng.PsToPx(90,90);
         Log.w( "DEBUG", "aaa x[] " + xy[0]);
         Log.w( "DEBUG", "aaa y[] " + xy[1]);
 
         // ImageViewオブジェクトの作成
-        ImageView imageView = new ImageView(context);
+        ImageView imageView = new ImageView(mContext);
 
         // 画像の設定
         imageView.setImageResource(ichienImageId);
 
         // タッチリスナーの設定
-     //   imageView.setOnTouchListener();
+        imageView.setOnTouchListener(this);
 
         // 画像のサイズの設定
-        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(150,150);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(100,100);
 
         // 表示座標の設定
         lp.leftMargin = (int)xy[0];
         lp.topMargin = (int)xy[1];
 
-
-        float xy2[] = CommonMng.PsToPx(15,80);
-
         // 画像の追加
-        layout.addView(imageView,lp);
+        mLayout.addView(imageView,lp);
 
-        // ImageViewオブジェクトの作成
-        ImageView imageView2 = new ImageView(context);
 
-        // 画像の設定
-        imageView2.setImageResource(R.drawable.ichien);
-
-        // タッチリスナーの設定
-//        imageView.setOnTouchListener(imageView.setOnTouchListener);
-
-        // 画像のサイズの設定
-        LinearLayout.LayoutParams lp2 = new LinearLayout.LayoutParams(150,150);
-
-        // 表示座標の設定
-        lp.leftMargin = (int)xy2[0];
-        lp.topMargin = (int)xy2[1];
-
-        // 画像の追加
-        layout.addView(imageView2,lp2);
     }
+    public boolean onTouch(View v, MotionEvent event) {
+        String currAction = "";
+        Log.w( "DEBUG_DATA", "v.toString() " + v.toString());
+        Log.w( "DEBUG_DATA", "v.getId() " + v.getId());
+        Log.w( "DEBUG_DATA", "v.getResources() " + v.getResources());
 
+        // イベントの状態を調べる
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+
+
+            case MotionEvent.ACTION_DOWN:
+                Log.w( "DEBUG_DATA", "DOWN2");
+                currAction = "DOWN";
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.w( "DEBUG_DATA", "MOVE2");
+                currAction = "MOVE";
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.w( "DEBUG_DATA", "UP2");
+                currAction = "UP";
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                Log.w( "DEBUG_DATA", "CANCEL2");
+                currAction = "CANCEL";
+                break;
+        }
+
+        return true;
+    }
 
 }
--- app/src/main/java/com/example/shinji/changegame/GameActivity.java
+++ app/src/main/java/com/example/shinji/changegame/GameActivity.java
@@ -1,33 +1,47 @@
 package com.example.shinji.changegame;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.view.View.OnTouchListener;
+
+import static java.security.AccessController.getContext;
 
 /**
  * Created by shinji on 2017/09/07.
  */
 
 public class GameActivity extends AppCompatActivity {
+    static int ichienImageId = R.drawable.ichien;
+    static Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        View decor = this.getWindow().getDecorView();
+        decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE);
 
+        mContext = this;
         // 表示したいレイアウト
         LinearLayout layout = (LinearLayout)findViewById(R.id.over_layout);
+        //CoinMng.CoinInit(this,layout);
 
-        CoinMng.CoinInit(this,layout);
-
-
+        CoinMng coinMng = new CoinMng(this,layout);
+        coinMng.CoinInit();
 
     }
+
 
 
 }
